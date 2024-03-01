5042570732d21af40350b76fc52abdb685ac3e67
PrOCaT20 <161882748+PrOCaT20@users.noreply.github.com>
2024-03-01 20:48:38 +0100

    Add files via upload

---
+++ GameLocalization/LocaleApplication.cs
@@ -0,0 +1,8 @@
+public class LocaleApplication
+{ 
+    public const string EN = "English";
+    public const string RU = "Russian";
+    public const string CA = "Catalan";
+    public const string UA = "Ukrainian";
+    public const string ES = "Spanish";
+}
