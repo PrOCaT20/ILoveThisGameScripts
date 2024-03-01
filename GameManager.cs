e9291dd793f2240aed5302cee1b7eaf5e656eac1
PrOCaT20 <161882748+PrOCaT20@users.noreply.github.com>
2024-03-01 22:26:23 +0100

    Update GameManager.cs

--- GameManager.cs
+++ GameManager.cs
@@ -7,69 +7,72 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
-    private Grid[] gridMaps;
-    public SpriteRenderer[] OtherBorders;
-    private SpriteRenderer PlayerRenderer;
-    private GameObject Door;
-    private Text[] ColorText;
-    private TextMeshPro[] ColorTextTMP;
-    public Image[] ImageColor;
-    private GameObject[] SoundEmitter;
-    public GameObject tipManager;
-    private GameObject tipButton;
-    public float tipSeconds = 3f;
-    public bool allowReload = true;
-    private Text levelText;
-    private static GameManager _instance;
-    public AudioSource[] otherSoundsCotroll;
+    private Grid[] gridMaps; // Mapa
+    public SpriteRenderer[] OtherBorders; // Imatges del mapa o altes parts
+    private SpriteRenderer PlayerRenderer; // Renderer del player 
+    private GameObject Door; // la porta
+    private Text[] ColorText; // TOT el text del nivell
+    private TextMeshPro[] ColorTextTMP;  // TOT el text del nivell
+    public Image[] ImageColor; // altres imatges no existents al mapa
+    private GameObject[] SoundEmitter; // objectes que treuen so
+    public GameObject tipManager; // Manager dels tips
+    private GameObject tipButton; // Butto per obrir tip
+    public float tipSeconds = 3f; // espera de segons avans de mostrar el tip
+    public bool allowReload = true; // Es permet reiniciar el nivell (s'utilita poc)
+    private Text levelText; // el text que mostra a quin nivell estem
+    private static GameManager _instance; // GameManager en si
+    public AudioSource[] otherSoundsCotroll; // altres objectes que treuen so no existents al mapa
 
-    private Sprite OpenDoorSprite, CloseDoorSprite;
-    private AudioClip OpenDoorSound, CloseDoorSound;
+    private Sprite OpenDoorSprite, CloseDoorSprite; // imatges de la porta oberta i tancada
+    private AudioClip OpenDoorSound, CloseDoorSound; // sons d'obrir i tancar la porta
 
-    public static GameManager Instance { get { if (_instance == null) { _instance = FindObjectOfType<GameManager>(); } return _instance; } }
+    public static GameManager Instance { get { if (_instance == null) { _instance = FindObjectOfType<GameManager>(); } return _instance; } } // Possibilitat d'acces al GameManager desde altres scripts, com el Jugador
 
-    void Awake()
+    void Awake() // El primer frame fes aixo una vegada:
     {
+    //Posar a quin nivell estem mirant el nom de la escena
         levelText = GameObject.Find("LevelInt").GetComponent<Text>();
            if (levelText != null)
                levelText.text = LocalizationManager.Instance.GetTextForKey("level") + " " + SceneManager.GetActiveScene().name;
 
+    //inicialitzacio del TipManager
         tipManager.SetActive(true);
         tipButton = GameObject.Find("TipButton");
 
+    //inicialitzacio del propi tip
         if (PlayerPrefs.GetInt("lvlTip" + SceneManager.GetActiveScene().name) == 0)
         {
             tipButton.SetActive(false);
             StartCoroutine(SetTip());
         } else if(tipButton != null) { tipButton.SetActive(true);}
 
-        SoundEmitter = GameObject.FindGameObjectsWithTag("SoundEmitter");
-        foreach (GameObject soundEmitter in SoundEmitter) soundEmitter.GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("SoundValue");
 
-        if(otherSoundsCotroll != null) foreach (AudioSource sound in otherSoundsCotroll) sound.volume = PlayerPrefs.GetFloat("SoundValue");
+        SoundEmitter = GameObject.FindGameObjectsWithTag("SoundEmitter"); // troba tots els objectes que emetin so i...
+        foreach (GameObject soundEmitter in SoundEmitter) soundEmitter.GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("SoundValue"); // posals'hi el volumen guardat a la memoria
 
-        ColorText = FindObjectsOfType<Text>();
-        ColorTextTMP = FindObjectsOfType<TextMeshPro>();
-        OpenDoorSprite = Resources.Load<Sprite>("Sprites/OpenDoor");
+        if(otherSoundsCotroll != null) foreach (AudioSource sound in otherSoundsCotroll) sound.volume = PlayerPrefs.GetFloat("SoundValue"); // el mateix
+
+        ColorText = FindObjectsOfType<Text>(); // trobar textos
+        ColorTextTMP = FindObjectsOfType<TextMeshPro>(); // Trobar textos
+        OpenDoorSprite = Resources.Load<Sprite>("Sprites/OpenDoor"); // cargar imatges de la porta desde arxius de joc
         CloseDoorSprite = Resources.Load<Sprite>("Sprites/CloseDoor");
-        Door = GameObject.Find("Door");
-        if(Door != null) Door.GetComponent<SpriteRenderer>().color = GetBorderColor();
-        OpenDoorSound = Resources.Load<AudioClip>("OpenDoorSound");
+        Door = GameObject.Find("Door"); // trobar l'objecte de la porta
+        OpenDoorSound = Resources.Load<AudioClip>("OpenDoorSound");  // cargar sons de la porta desde arxius de joc
         CloseDoorSound = Resources.Load<AudioClip>("CloseDoorSound");
-        PlayerRenderer = GameObject.Find("Player").GetComponent<SpriteRenderer>();
+        PlayerRenderer = GameObject.Find("Player").GetComponent<SpriteRenderer>(); // trobar renderer del jugador
 
-        if (PlayerPrefs.HasKey("HexBackgroundColor"))
+        if (PlayerPrefs.HasKey("HexBackgroundColor")) // aplicar color al fondo
             Camera.main.backgroundColor = HexToColor(PlayerPrefs.GetString("HexBackgroundColor"));
 
-        gridMaps = FindObjectsOfType<Grid>();
+        gridMaps = FindObjectsOfType<Grid>(); // trobar mapa
 
-        if (PlayerPrefs.HasKey("HexColor"))
+        if (PlayerPrefs.HasKey("HexColor"))// trobar HEX CODE del color triat
             UpdateColorAll(HexToColor(PlayerPrefs.GetString("HexColor")));
 
-        if (PlayerRenderer != null) PlayerRenderer.color = HexToColor(PlayerPrefs.GetString("HexColor"));
+        if (PlayerRenderer != null) PlayerRenderer.color = HexToColor(PlayerPrefs.GetString("HexColor")); // aplicar color al jugador
     }
 
-    void UpdateColorAll(Color color) => UpdateColorBorders(color);
+    void UpdateColorAll(Color color) => UpdateColorBorders(color); // inicialitzacio de colors
 
     public void OpenTip()
     {
@@ -77,7 +80,7 @@ public class GameManager : MonoBehaviour
         tipManager.SetActive(true);
     }
 
-    void UpdateColorBorders(Color color)
+    void UpdateColorBorders(Color color) // metode molt gran que simplement agafa TOTS els objectes i els hi posa el color
     {
         void UpdateColor(Graphic[] graphics)
         {
@@ -122,40 +125,41 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    private void Update() { if (Input.GetKeyDown(KeyCode.R) && allowReload) ReloadLevel(); }
+    private void Update() { if (Input.GetKeyDown(KeyCode.R) && allowReload) ReloadLevel(); } // si es prem el boto R, reinicia el nivell
 
-    IEnumerator SetTip()
+    IEnumerator SetTip() // temps que es tarda perque el tip sigui disponible
     {
         yield return new WaitForSeconds(tipSeconds);
         tipButton.SetActive(true);
         PlayerPrefs.SetInt("lvlTip" + SceneManager.GetActiveScene().name, 1);
     }
 
-    public void ReloadLevel()
+    public void ReloadLevel() // reinici nivell
     {
-        PlayerPrefs.SetInt("ReloadsCount", PlayerPrefs.GetInt("ReloadsCount") + 1);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        PlayerPrefs.SetInt("ReloadsCount", PlayerPrefs.GetInt("ReloadsCount") + 1); // sumar al comptador de reinicis una unitat
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // cargar la mateixa escena que es troba ara el USER
     }
 
-    private Color HexToColor(string hex)
+    private Color HexToColor(string hex) // convertir HEX COLOR CODE
     {
         Color color = Color.black;
         ColorUtility.TryParseHtmlString(hex, out color);
         return color;
     }
 
-    public Color GetBorderColor()
+    public Color GetBorderColor() // metode accesible desde altres scripts per aconseguir el color guardat a la memoria
     {
         Color colorItNeeds = HexToColor(PlayerPrefs.GetString("HexColor"));
         return colorItNeeds;
     }
 
-    public Color GetBgColor()
+    public Color GetBgColor() // metode accesible desde altres scripts per aconseguir el color del fons guardat a la memoria
     {
         Color colorItNeeds = HexToColor(PlayerPrefs.GetString("HexBackgroundColor"));
         return colorItNeeds;
     }
 
+//metodes per obrir i tancar la porta
     public void OpenDoor() => HandleDoor(true, OpenDoorSprite, OpenDoorSound);
     public void CloseDoor() => HandleDoor(false, CloseDoorSprite, CloseDoorSound);
 
