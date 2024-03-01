8be7a85580abd3d5d3629b132212213fe69c3f11
PrOCaT20 <161882748+PrOCaT20@users.noreply.github.com>
2024-03-01 20:47:13 +0100

    Delete LocaleApplication.cs

--- LocaleApplication.cs
+++
@@ -1,8 +0,0 @@
-public class LocaleApplication
-{ 
-    public const string EN = "English";
-    public const string RU = "Russian";
-    public const string CA = "Catalan";
-    public const string UA = "Ukrainian";
-    public const string ES = "Spanish";
-}
